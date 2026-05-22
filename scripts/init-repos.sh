8de2267dfc85bd6fecf8e9482d09305dcc4e2a46
Tai Ho (tai271828) <e@tai271828.me>
2026-05-23 01:34:16 +0200

    correct relative path

--- scripts/init-repos.sh
+++ scripts/init-repos.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-push ../
+push ../../
 git clone git@github.com:ML4PhA-G11/simulators.git
 git clone git@github.com:ML4PhA-G11/model-experiments.git
 git clone git@github.com:ML4PhA-G11/Apply-NN-KarmanVortexStreet.git
+ln -s ../data ./
 popd
 
-ln -s ../data ./
