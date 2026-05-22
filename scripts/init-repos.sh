13e266ccdf029475b61ac66272039c0b9b669a13
Tai Ho (tai271828) <e@tai271828.me>
2026-05-23 01:30:19 +0200

    create init-repos.sh

---
+++ scripts/init-repos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+push ../
+git clone git@github.com:ML4PhA-G11/simulators.git
+git clone git@github.com:ML4PhA-G11/model-experiments.git
+git clone git@github.com:ML4PhA-G11/Apply-NN-KarmanVortexStreet.git
+popd
+
+ln -s ../data ./
