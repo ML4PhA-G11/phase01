bdc157d4d71163e274ce899249a7558ed8e1cfc5
Tai Ho (tai271828) <e@tai271828.me>
2026-05-23 01:37:08 +0200

    correct relative path: fix typo

--- scripts/init-repos.sh
+++ scripts/init-repos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-push ../../
+pushd ../
 git clone git@github.com:ML4PhA-G11/simulators.git
 git clone git@github.com:ML4PhA-G11/model-experiments.git
 git clone git@github.com:ML4PhA-G11/Apply-NN-KarmanVortexStreet.git
